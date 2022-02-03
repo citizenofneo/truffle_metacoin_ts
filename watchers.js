0a876ce1f1a61013dfdbd053b69100aceb8b2104
citizenofneo <citizenofneo@gmail.com>
2022-02-04 00:33:42 +0300

    watcher fix

--- watchers.js
+++ watchers.js
@@ -1,30 +1,30 @@
 const fs = require('fs')
 const { exec } = require('child_process');
 
+cleanDirs()
 setWatcher('./contracts', 'truffle compile && npm run generate-types')
 
 function setWatcher(watchedDir, cmd) {
     console.log('[Start watch]:', ...arguments)
+
     const useCmd = debounce(filename => {
-        console.log(`${filename} file Changed`)
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`error: ${watchedDir} ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.error(`stderr: ${watchedDir} ${stderr}`);
-                return;
-            }
-            console.log(`stdout:\n${stdout}`);
-        })
+        console.log(`${filename} file was Changed`)
+        exec(cmd, (error, stdout) => console.log(`stdout:\n${stdout}`))
     }, 1000)
-    fs.watch(watchedDir, (event, filename) => {
-        if (filename) {
-            useCmd(filename)
-        }
-    })
+
+    fs.watch(watchedDir, (event, filename) => filename && useCmd(filename))
+
     useCmd('pre')
+}
+
+function cleanDirs(dirs = ['./build', './types', './bin']) {
+    for (let dirName of dirs) {
+        if (fs.existsSync(dirName)) {
+            console.log('Clean', dirName)
+            fs.rmSync(dirName, { recursive: true, force: true })
+        }
+    }
+
 }
 
 function debounce(f, ms) {
